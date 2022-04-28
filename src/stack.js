b29b97aa243138ede84e2384058e81869952264a
oleg <zerex21@icloud.com>
2022-04-28 14:34:57 +0300

    feat:add stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.items = [];
+    this.count = 0;
+  }
+
+  push(element) {
+    this.items[this.count] = element;
+    this.count += 1;
+    return this.count - 1;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   if(this.count == 0) return undefined;
+    let deleteItem = this.items[this.count - 1];
+    this.count -=1;
+    return deleteItem;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return this.items[this.count - 1]
   }
 }
 
